23193504043c4ca097d63a09fb703437dcad5da5
Juan Aliaga <juanialiaga4@gmail.com>
2023-07-21 13:49:18 -0300

    agregado de clase

---
+++ js/items.js
@@ -0,0 +1,16 @@
+class items{
+
+    id;
+    nombre;
+    marca;
+    precio;
+    imagen;
+
+    constructor(id,nombre,marca,precio,imagen){
+        this.id = id
+        this.nombre = nombre
+        this.marca = marca
+        this.precio = precio
+        this.imagen = imagen
+    }
+}
